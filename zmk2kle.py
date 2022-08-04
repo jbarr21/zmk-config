950dc229c6ae8d3882d2f390ea63af7a6874f7c9
James Barr <barr.james.r@gmail.com>
2022-08-03 23:48:20 -0700

    Parse using tree-sitter

--- zmk2kle.py
+++ zmk2kle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import argparse
+import configparser
+from dataclasses import dataclass
+from logging import root
 import os
 import re
 import time
@@ -6,7 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from sys import argv
 from textwrap import dedent
-import argparse
+from tree_sitter import Language, Parser
 import urllib.parse
 
 # KLE JSON for a 3x6+3
@@ -19,86 +23,52 @@ KLE_LAYOUT = dedent('''
 
 MODS = ['LGUI', 'LSHFT', 'LALT', 'LCTRL', 'RGUI', 'RSHFT', 'RALT', 'RCTRL']
 kle_chars_needing_escaping = ['/', ';', '@', '&', '_', '=']
-zmk_to_name = {
-  'SEMI': ';',
-  'BSLH': '\\',
-  'APOS': '\'',
-  'COMMA': ',',
-  'DOT': '.',
-  'FSLH': '/',
-  'SPACE': 'Space',
-  'BSPC': '⌫',
-  'RET': '⏎',
-  'EXCL': '!',
-  'AT': '@',
-  'HASH': '#',
-  'DLLR': '$',
-  'PRCNT': '%',
-  'CARET': '^',
-  'AMPS': '&',
-  'STAR': '*',
-  'PLUS': '+',
-  'UNDER': '_',
-  'EQUAL': '=',
-  'MINUS': '-',
-  'GRAVE': '`',
-  'PIPE': '|',
-  'LT': '<',
-  'LBKT': '[',
-  'LBRC': '{',
-  'LPAR': '(',
-  'TILDE': '~',
-  'QMARK': '?',
-  'RPAR': ')',
-  'RBRC': '}',
-  'RBKT': ']',
-  'GT': '>',
-  'C_PREV': '|◀',
-  'C_STOP': '■',
-  'C_PP': '▶',
-  'C_NEXT': '|▶',
-  'LEFT': '←',
-  'DOWN': '↓',
-  'UP': '↑',
-  'RIGHT': '→',
-  'KP_DOT': '.',
-  'LGUI': '⌘',
-  'RGUI': '⌘',
-  'LSHFT': '⇧',
-  'RSHFT': '⇧',
-  'LALT': '⌥',
-  'RALT': '⌥',
-  'LCTRL': '⎈',
-  'RCTRL': '⎈',
-  'LG': '⌘',
-  'RG': '⌘',
-  'LS': '⇧',
-  'RS': '⇧',
-  'LA': '⌥',
-  'RA': '⌥',
-  'LC': '⎈',
-  'RC': '⎈',
-}
+zmk_to_name = { }
 
+@dataclass
+class KeyAction:
+    type: str
+    tap: str
+    hold: str
+
+@dataclass
 class Key:
-    def __init__(self, code, aliases):
-        self.hold = None
-        args = code.split(' ')
-        action = args[0]
+    cells: list[str]
 
-        if action == '&kp':
-            self.tap = self.name(args[1])
-        elif action == '&mt' or action == '&lt':
-            self.hold = self.name(args[1])
-            self.tap = self.name(args[2])
-        elif action == '&sk' or action == '&sl':
-            self.tap = self.name(args[1]) + '<br>' + action.replace('&', '').upper()
-        elif action == '&none':
-            self.tap = ''
+    def label(self, encoded=False) -> str:
+        action = self._action()
+        label = action.tap
+        if encoded:
+            for ch in [c for c in kle_chars_needing_escaping if c in label]:
+                label = label.replace(ch, '/' + ch)
+
+        if action.hold is not None:
+            label += f"\n\n\n\n{action.hold}"
+
+        if not encoded:
+            label = label.replace('\\', '\\\\').replace('\n', '\\n')
+ 
+        return urllib.parse.quote(label).replace('/', '%2F') if encoded else label
+
+    def _action(self) -> KeyAction:
+        type = self.cells[0].text.decode()[1:]
+        args = [self._legend(x.text.decode()) for x in self.cells[1:]]
+
+        if type == 'kp':
+            return KeyAction(type=type, tap=args[0], hold=None)
+        elif type in ['mt', 'lt']:
+            return KeyAction(type=type, tap=args[1], hold=args[0])
+        elif type in ['sk', 'sl']:
+            return KeyAction(type=type, tap=f"{args[0]}<br>{type.upper()}", hold=None)
+        elif type == 'trans':
+            return KeyAction(type=type, tap='___', hold=None) 
+        elif type == 'none':
+            return KeyAction(type=type, tap='', hold=None)
         else:
-            self.tap = self.name(code)
+            tap = self._legend((type if len(args) == 0 else ' '.join(args)).upper())
+            return KeyAction(type=type, tap=tap, hold=None)
 
-    def name(self, code):
+    def _legend(self, code) -> str:
         code = zmk_to_name.get(code, code)
         if len(code) == 2 and code.startswith('N'):
             code = code[1:]
@@ -111,41 +81,24 @@ class Key:
             code = code.replace('_', ' ')
         return code
 
-    def label(self, encoded=False):
-        label = self.tap
-        if encoded:
-            for ch in [c for c in kle_chars_needing_escaping if c in label]:
-                label = label.replace(ch, '/' + ch)
-
-        if self.hold is not None:
-            label += f"\n\n\n\n{self.hold}"
-
-        if not encoded:
-            label = label.replace('\\', '\\\\').replace('\n', '\\n')
- 
-        return urllib.parse.quote(label).replace('/', '%2F') if encoded else label
-
-
+@dataclass
 class Layer:
-    Key = re.compile(r'&\w+(?:.(?!(&|$)))*')
-    
-    def __init__(self, data, aliases):
-        self.name = re.search(r'(?<=label = ")\w+(?=";)', data).group()
-        self.keys = []
-        for key in Layer.Key.finditer(data):
-            self.keys += [Key(key.group().strip(), aliases)]
+    name: str
+    label: str
+    keys: list[Key]
 
     def to_kle(self, encoded=False):
-        header = '''[{d:true,w:5,a:6,f:5}, %s],''' % (f"={self.name}" if encoded else f"\"{self.name}\"")
+        layer_name = (self.label if self.label is not None else self.name.upper()).replace('_', '%20' if encoded else ' ')
+        header = '''[{d:true,w:5,a:6,f:5}, %s],''' % (f"={layer_name}" if encoded else f"\"{layer_name}\"")
         data = f"{header}\n{KLE_LAYOUT.strip()}"
         if encoded:
-            data = self.kle_encode(data)
-        return self.add_keys(data, '""', [key.label(encoded=encoded) for key in self.keys], encoded)
+            data = self._kle_encode(data)
+        return self._add_keys(data, '""', [key.label(encoded=encoded) for key in self.keys], encoded)
 
     def to_kle_url(self):
         return self.to_kle(encoded=True).replace('\n', '')
 
-    def add_keys(self, text, target, replacements, encoded):
+    def _add_keys(self, text, target, replacements, encoded):
         item = 0
         index = text.find('""', 0)
         while index >= 0:
@@ -160,7 +113,7 @@ class Layer:
             item+=1
         return text
 
-    def kle_encode(self, text):
+    def _kle_encode(self, text):
         semicolon = urllib.parse.quote(';')
         chars = {
             ',': '&',
@@ -174,17 +127,11 @@ class Layer:
             text = text.replace(k, v)
         return text
 
-class Layout:
-    Layer = re.compile(r'(default_layer|layer_\w+) {.*?};', re.DOTALL | re.MULTILINE)
+@dataclass
+class Keymap:
+    layers: list[Layer]
+    defines: dict[str, str]
 
-    def __init__(self, file):
-        self.layers = []
-        self.aliases = {}
-        with open(file, 'r') as f:
-            text = f.read()
-            for layer in Layout.Layer.finditer(text):
-                self.layers += [Layer(layer.group(), self.aliases)]
-    
     def to_kle(self):
         return '\n'.join([layer.to_kle() for layer in self.layers])
 
@@ -228,6 +175,65 @@ def download_wait(directory, timeout, nfiles=None):
         seconds += 1
     return seconds
 
+def parse_keymap(keymap_path):
+    keymap_text = None
+    with open(keymap_path, 'r') as f:
+        keymap_text = f.read()
+
+    so_path = 'devicetree.so' if not os.environ.get('GITHUB_WORKSPACE', None) else (os.environ.get('HOME')+'/.cache/tree-sitter/lib/devicetree.so')
+    DTS_LANGUAGE = Language(so_path, 'devicetree')
+    parser = Parser()
+    parser.set_language(DTS_LANGUAGE)
+    tree = parser.parse(bytes(keymap_text, 'utf8'))
+    
+    slash = [c for c in tree.root_node.children if c.type == 'node' and node_name(c) == '/'][0]
+    keymap = [c for c in slash.children if c.type == 'node' and node_name(c) == 'keymap'][0]
+    layers = [c for c in keymap.children if c.type == 'node']
+    layers = [c for c in layers if len([p for p in c.children if node_name(p) == 'bindings']) > 0]
+    keymap_layers = []
+
+    for layer in layers:
+        name = node_name(layer)
+        bindings = [c for c in layer.children if node_name(c) == 'bindings'][0]
+        label = [x.text.decode().replace('"', '') for x in flatten([c.children for c in layer.children if node_name(c) == 'label']) if x.type == 'string_literal']
+        label = next(iter(label), None)
+        layer_cells = [c for c in bindings.children if c.type == 'integer_cells'][0].children
+        keys = []
+        cells = []
+        for i, cell in enumerate(layer_cells):
+            if cell.type != 'comment' and node_name(cell) is not None and node_name(cell) != '<':
+                cells += [cell]
+
+            if (i == len(layer_cells) - 1 or layer_cells[i + 1].type == 'reference') and len(cells) > 0: 
+                keys += [Key(cells)]
+                cells = []
+
+        keymap_layers += [Layer(name, label, keys)]
+       
+    return Keymap(keymap_layers, {})
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+def node_name(node):
+    if node is None or not hasattr(node, 'type'):
+        return None
+    elif node.type == 'identifier':
+        return node.text.decode()
+    else:
+        ids = [c for c in node.children if c.type == 'identifier']
+        refs = [c for c in node.children if c.type == 'reference']
+        props = [c for c in node.children if c.type == 'property']
+        return node_name(next(iter(ids + refs + props), None))
+
+def load_config():
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    with open('legends.properties', 'r') as f:
+        config.read_file(f)
+        if 'legends' in config._sections:
+            zmk_to_name.update(config._sections.get("legends"))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert ZMK layout to KLE image')
     parser.add_argument('keymap', type=str, help='Path to keymap file')
@@ -235,15 +241,9 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--link_only', help='Show KLE link instead of KLE raw data', action='store_true')
     args = parser.parse_args()
 
-    try:
-        layout = Layout(args.keymap)
-        if args.link_only:
-            print(layout.to_kle_url())
-        else:
-            print(layout.to_kle())
+    load_config()
+    keymap = parse_keymap(args.keymap)
+    print(keymap.to_kle_url() if args.link_only else keymap.to_kle())
 
-        if args.save_image in argv:
-            save_image(layout.to_kle_url())
-
-    except Exception as ex:
-        exit(str(ex))
+    if args.save_image in argv:
+        save_image(keymap.to_kle_url())
