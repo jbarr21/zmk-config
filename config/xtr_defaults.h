29d0228b8ac8e016d8278192fa5b6113670f5eb5
James Barr <barr.james.r@gmail.com>
2022-12-18 01:28:43 -0800

    Switch to nodefree config

---
+++ config/xtr_defaults.h
@@ -0,0 +1,32 @@
+// The default layout has 36 keys. Additional keys can be added by pre-setting any of
+// the macros defined in this file to one or more keys before sourcing this file.
+
+#if !defined XTR_LTO
+    #define XTR_LTO
+#endif
+#if !defined XTR_LMO
+    #define XTR_LMO
+#endif
+#if !defined XTR_LBO
+    #define XTR_LBO
+#endif
+
+#if !defined XTR_RTO
+    #define XTR_RTO
+#endif
+#if !defined XTR_RMO
+    #define XTR_RMO
+#endif
+#if !defined XTR_RBO
+    #define XTR_RBO
+#endif
+
+#if !defined XTR_MH
+    #define XTR_MH
+#endif
+#if !defined XTR_LH
+    #define XTR_LH
+#endif
+#if !defined XTR_RH
+    #define XTR_RH
+#endif
